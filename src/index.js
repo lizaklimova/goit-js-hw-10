fb6494e9085b4057504f56048abeeccb19860b11
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-10-03 12:56:50 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import SlimSelect from 'slim-select';
+
 import Notiflix from 'notiflix';
-
-const select = new SlimSelect({
-  select: document.querySelector('.breed-select'),
-
-  settings: {
-    placeholderText: 'Select the breed of cat 🐈',
-  },
-});
 
 const refs = {
   selectRef: document.querySelector('.breed-select'),
@@ -16,34 +9,51 @@ const refs = {
 };
 
 refs.selectRef.addEventListener('change', onSelectChange);
+showContent('none');
 
 // ~ Парсинг опцій
 fetchBreeds()
   .then(data => {
-    select.setData(optionsMarkup(data));
+    refs.selectRef.insertAdjacentHTML('beforeend', optionsMarkup(data));
+    new SlimSelect({
+      select: document.querySelector('.breed-select'),
+      settings: {
+        placeholderText: 'Select the breed of cat 🐈',
+        // ! Підкажіть, будь ласка, чому плейсхолдер не відображається? Селект відображає першу опцію!
+      },
+    });
   })
-  .catch(error => console.log(error));
+  .catch(() =>
+    Notiflix.Notify.failure(
+      'Ooops!😮 Something went wrong! Try reloading the page! ❗️'
+    )
+  );
 
 // ~ Розмітка опцій випадаючого меню
 function optionsMarkup(arr) {
-  return arr.map(({ name, id }) => ({ text: name, value: id }));
+  return arr.map(({ name, id }) => {
+    return `<option value=${id}>${name}</option>`;
+  });
 }
 
 // ~ Ріпейнт обраного котика
 function onSelectChange(event) {
   refs.containerRef.innerHTML = '';
-  const id = event.target.value;
   Notiflix.Loading.circle();
+
+  const id = event.target.value;
 
   fetchCatByBreed(id)
     .then(data => {
       const baseDist = data[0].breeds[0];
 
       const markup = `
-        <img alt="Cat ${baseDist.name}" src="${data[0].url} />
-        <h1 class="cat-info-title">${baseDist.name}</h1>
+      <img alt="Cat ${baseDist.name}" src="${data[0].url}" />
+        <div class="content"> <h1 class="cat-info-title">${baseDist.name}🐈</h1>
         <p class ="cat-info-descr">${baseDist.description}</p>
-        <p class = "cat-info-temperament"><span>Temperament:</span>${baseDist.temperament}</p>`;
+        <p class = "cat-info-temperament"><span>😊Temperament:&#x20;</span> ${baseDist.temperament}</p></div>
+      `;
+      showContent('block');
       refs.containerRef.innerHTML = markup;
     })
     .catch(() =>
@@ -54,4 +64,8 @@ function onSelectChange(event) {
     .finally(() => {
       Notiflix.Loading.remove();
     });
+}
+
+function showContent(display) {
+  refs.containerRef.style.display = display;
 }
