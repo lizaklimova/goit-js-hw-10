c49d19c874a5c9afbc514c9736bb30f40a9b964b
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-10-03 11:02:07 +0300

    1

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,22 +5,10 @@ axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
   return axios.get('/breeds').then(response => response.data);
-  //   return fetch(`${BASE_URL}/breeds`).then(response => {
-  //     if (!response.ok) {
-  //       throw new Error(`Sorry, the error happened ${response.statusText}`);
-  //     }
-  //     return response.json();
-  //   });
 }
 
 export function fetchCatByBreed(breedId) {
   return axios
     .get(`/images/search?breed_id=${breedId}`)
     .then(response => response.data);
-  //   return fetch(`${BASE_URL}?breed_id=${breedId}`).then(response => {
-  //     if (!response.ok) {
-  //       throw new Error(`Sorry, the error happened ${response.statusText}`);
-  //     }
-  //     return response.json();
-  //   });
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+const select = new SlimSelect({
+  select: document.querySelector('.breed-select'),
+
+  settings: {
+    placeholderText: 'Select the breed of cat 🐈',
+  },
+});
 
 const refs = {
   selectRef: document.querySelector('.breed-select'),
   containerRef: document.querySelector('.cat-info'),
-  loaderRef: document.querySelector('.loader'),
-  errorRef: document.querySelector('.error'),
 };
 
 refs.selectRef.addEventListener('change', onSelectChange);
@@ -12,40 +20,38 @@ refs.selectRef.addEventListener('change', onSelectChange);
 // ~ Парсинг опцій
 fetchBreeds()
   .then(data => {
-    refs.selectRef.insertAdjacentHTML('beforeend', optionsMarkup(data));
+    select.setData(optionsMarkup(data));
   })
   .catch(error => console.log(error));
 
 // ~ Розмітка опцій випадаючого меню
 function optionsMarkup(arr) {
-  return arr
-    .map(({ name, id }) => {
-      return `<option value="${id}"> ${name} </option>`;
-    })
-    .join('');
+  return arr.map(({ name, id }) => ({ text: name, value: id }));
 }
 
 // ~ Ріпейнт обраного котика
 function onSelectChange(event) {
-  refs.containerRef.textContent = '';
+  refs.containerRef.innerHTML = '';
   const id = event.target.value;
-  toggleClassList(refs.loaderRef, 'inactive', 'active');
+  Notiflix.Loading.circle();
 
   fetchCatByBreed(id)
     .then(data => {
       const baseDist = data[0].breeds[0];
 
       const markup = `
-        <img alt="Cat ${baseDist.name}" src="${data[0].url}" />
+        <img alt="Cat ${baseDist.name}" src="${data[0].url} />
         <h1 class="cat-info-title">${baseDist.name}</h1>
         <p class ="cat-info-descr">${baseDist.description}</p>
         <p class = "cat-info-temperament"><span>Temperament:</span>${baseDist.temperament}</p>`;
       refs.containerRef.innerHTML = markup;
     })
-    .catch(error => console.log(error))
-    .finally(() => toggleClassList(refs.loaderRef, 'active', 'inactive'));
-}
-
-function toggleClassList(ref, cl1, cl2) {
-  ref.classList.replace(cl1, cl2);
+    .catch(() =>
+      Notiflix.Notify.failure(
+        'Ooops!😮 Something went wrong! Try reloading the page! ❗️'
+      )
+    )
+    .finally(() => {
+      Notiflix.Loading.remove();
+    });
 }
